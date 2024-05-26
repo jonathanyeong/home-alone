3d0683b308557b881ec5cdbd328ff10dbef2cb76
Jonathan Yeong <jonathanyeong@gmail.com>
2024-05-26 18:32:45 -0400

    Add Dog model, associate it to a trainer
    A trainer can have many dogs.
    Update seed file to add a dog with the trainer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_26_221737) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_26_222815) do
+  create_table "dogs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.bigint "trainer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["trainer_id"], name: "index_dogs_on_trainer_id"
+  end
+
   create_table "trainers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +31,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_26_221737) do
     t.index ["reset_password_token"], name: "index_trainers_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "dogs", "trainers"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 puts "Creating a new trainer"
-Trainer.create(email: "doggyowner@anxiety.com", password: "password", password_confirmation: "password")
+trainer = Trainer.create!(email: "doggyowner@anxiety.com", password: "password", password_confirmation: "password")
+puts "Adding dog for the trainer: #{trainer.email}"
+trainer.dogs.create!(name: "Albie")
